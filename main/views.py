e95bc0de49aeeb33555cf1e8e5b17b7d8b6f54cc
fikaradrv <fikar2006@gmail.com>
2024-10-07 19:15:24 +0700

    feat: added protection and make new button for adding by ajax

--- main/views.py
+++ main/views.py
@@ -9,16 +9,19 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
+    
     context = {
         'npm' : '2306203873',
         'name': request.user.username,
         'class': 'PBP C',
-        'product_entries' : product_entries,
+        
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -37,11 +40,11 @@ def create_product_entry(request):
     return render(request, "create_product_entry.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -102,3 +105,24 @@ def delete_product(request, id):
     product = Product.objects.get(pk = id)
     product.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    stock = request.POST.get("stock")
+    description = strip_tags(request.POST.get("description"))
+    rating = request.POST.get("rating")
+    user = request.user
+
+    new_product = Product(
+        name=name, price=price,
+        stock=stock,
+        description=description,
+        rating=rating,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
